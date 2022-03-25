7affaf3b7e43d805fed7c4b76fe1e2ade97a89e0
ScottAgirs <scott@ijs.to>
2022-03-25 15:44:33 +0100

    refactor(Types): Add AuthTemplateOptions to /templates
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- packages/keystone-6-oauth/src/templates/auth.ts
+++ packages/keystone-6-oauth/src/templates/auth.ts
@@ -1,5 +1,6 @@
-import ejs from 'ejs';
-import { AuthGqlNames } from '../types';
+import ejs from "ejs";
+import { NextAuthPageProps } from "../pages/NextAuthPage";
+import { AuthGqlNames } from "../types";
 
 const template = `
 import getNextAuthPage from 'keystone-6-oauth/pages/NextAuthPage';
@@ -7,40 +8,37 @@ import { query } from '.keystone/api';
 import keystoneConfig from '../../../../../keystone';
 
 export default getNextAuthPage({
-        identityField: '<%= identityField %>',
-        sessionData: '<%= sessionData %>',
-        listKey: '<%= listKey %>',
         autoCreate: <%= autoCreate %>,
-        sessionSecret: '<%= sessionSecret %>',
-        resolver: keystoneConfig.resolver,
+        identityField: '<%= identityField %>',
+        listKey: '<%= listKey %>',
+        pages: keystoneConfig.pages,
         providers: keystoneConfig.providers,
         query,
+        resolver: keystoneConfig.resolver,
+        sessionData: '<%= sessionData %>',
+        sessionSecret: '<%= sessionSecret %>',
     });
   `;
 
+type AuthTemplateOptions = NextAuthPageProps & { gqlNames: AuthGqlNames };
+
 export const authTemplate = ({
   gqlNames,
-  identityField,
-  sessionData,
-  listKey,
   autoCreate,
+  identityField,
+  listKey,
+  pages,
   resolver,
+  sessionData,
   sessionSecret,
-}: {
-  gqlNames: AuthGqlNames;
-  identityField: string;
-  sessionData: any;
-  listKey: string;
-  autoCreate: boolean;
-  resolver: Function;
-  sessionSecret: string;
-}) => {
+}: AuthTemplateOptions) => {
   const authOut = ejs.render(template, {
     gqlNames,
     identityField,
     sessionData,
     listKey,
     autoCreate,
+    pages,
     resolver,
     sessionSecret,
   });
