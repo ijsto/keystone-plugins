b2f0e93897bc52d5c14bfb11b5f17b8ff88722d1
ScottAgirs <scott@ijs.to>
2022-06-04 10:16:49 +0300

    chore(Conform): update NextAuthPage />
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- packages/keystone-6-oauth/src/pages/NextAuthPage.tsx
+++ packages/keystone-6-oauth/src/pages/NextAuthPage.tsx
@@ -1,44 +1,71 @@
-import NextAuth from 'next-auth';
+import NextAuth, { CookiesOptions, EventCallbacks, PagesOptions } from 'next-auth';
 import type { KeystoneListsAPI } from '@keystone-6/core/types';
 import { Provider } from 'next-auth/providers';
+import { JWTOptions } from 'next-auth/jwt';
 import { validateNextAuth } from '../lib/validateNextAuth';
 
-// Need to bring in correct props
-type NextAuthPageProps = {
-  identityField: string;
-  mutationName: string;
-  providers: Provider[];
-  query: KeystoneListsAPI<any>;
-  sessionData: string;
-  listKey: string;
+type CoreNextAuthPageProps = {
   autoCreate: boolean;
+  cookies?: Partial<CookiesOptions>;
+  events?: Partial<EventCallbacks>;
+  identityField: string;
+  jwt?: Partial<JWTOptions>;
+  listKey: string;
+  pages?: Partial<PagesOptions>;
+  providers?: Provider[];
+  resolver?: (args: { user: any; profile: any; account: any }) => {
+    [key: string]: boolean | string | number;
+  };
+  sessionData: string | undefined;
   sessionSecret: string;
-  resolver: Function;
 };
+
+type NextAuthGglProps = {
+  mutationName?: string;
+  query?: KeystoneListsAPI<any>;
+};
+
+export type NextAuthPageProps = CoreNextAuthPageProps & NextAuthGglProps;
 
 export default function NextAuthPage(props: NextAuthPageProps) {
   const {
+    autoCreate,
+    cookies,
+    events,
+    identityField,
+    jwt,
+    listKey,
+    pages,
     providers,
     query,
-    identityField,
-    sessionData,
-    listKey,
-    autoCreate,
-    // Can we do this?
     resolver,
+    sessionData,
     sessionSecret,
   } = props;
-  // TODO: (v1.1.0). https://github.com/ijsto/keystone-6-oauth/projects/1#card-78602004
+
+  if (!query) {
+    console.error('NextAuthPage got no query.');
+    return null;
+  }
+
+  if (!providers || !providers.length) {
+    console.error('You need to provide at least one provider.');
+    return null;
+  }
 
   const list = query[listKey];
   const queryAPI = query[listKey];
   const protectIdentities = true;
 
   return NextAuth({
-    secret: sessionSecret,
+    cookies,
     providers,
+    pages: pages || {},
+    events: events || {},
+    jwt: jwt || {},
+    secret: sessionSecret,
     callbacks: {
-      async signIn({ user, account, profile, ...rest }) {
+      async signIn({ user, account, profile }) {
         let identity;
         if (typeof user.id === 'string') {
           identity = user.id;
@@ -47,95 +74,91 @@ export default function NextAuthPage(props: NextAuthPageProps) {
         } else {
           identity = 0;
         }
-        const userInput = await resolver({ user, account, profile, ...rest });
-        console.log('signIn :: userInput', userInput);
+        const userInput = resolver ? await resolver({ user, account, profile }) : {};
 
-        const result = await validateNextAuth(
-          identityField,
-          identity,
-          protectIdentities,
-          queryAPI
-        );
+        const result = await validateNextAuth(identityField, identity, protectIdentities, queryAPI);
         // ID
         const data: any = {
-          // TODO: https://github.com/ijsto/keystone-6-oauth/projects/1#card-78601954
           [identityField]: identity,
           ...userInput,
         };
 
         if (!result.success) {
           if (!autoCreate) {
-            console.log(
-              '`autoCreate` if set to `false`, skipping user auto-creation'
-            );
+            console.log('`autoCreate` is set to `false`, skipping user auto-creation');
             return false;
           }
-          console.log(
-            '`autoCreate` if set to `true`, auto-creating a new user'
-          );
+          console.log('`autoCreate` is set to `true`, auto-creating a new user');
 
           const createUser = await list
             .createOne({ data })
-            .then((returned) => {
-              console.log('User Created', JSON.stringify(returned));
-              return true;
+            .then(returned => {
+              return { success: true, user: returned };
             })
-            .catch((error) => {
+            .catch(error => {
               console.log(error);
               throw new Error(error);
             });
           console.log('Created User', createUser);
-          return createUser;
+          return createUser.success;
         }
-        // await list.updateOne({where: {id: result.item.id}, data});
-        return result.success;
+        console.log('Data', data);
+
+        const updateUser = await list
+          .updateOne({ where: { id: result.item.id }, data })
+          .then(returned => {
+            return { success: true, user: returned };
+          })
+          .catch(error => {
+            console.log(error);
+            throw new Error(error);
+          });
+        return updateUser.success;
       },
       async redirect({ url }) {
         return url;
       },
       async session({ session, token }) {
-        const returnSession = {
-          ...session,
-          data: token.data,
-          subject: token.sub,
-          listKey: token.listKey,
-          itemId: token.itemId,
-        };
+        let returnSession = session;
+        if (!token.itemId) {
+          return { expires: '0' };
+        } else {
+          returnSession = {
+            ...session,
+            data: token.data,
+            subject: token.sub,
+            listKey: token.listKey as string,
+            itemId: token.itemId as string,
+          };
+        }
+        console.log('Session', returnSession);
+
         return returnSession;
       },
       async jwt({ token }) {
         const identity = token.sub as number | string;
-        if (!token.itemId) {
-          const result = await validateNextAuth(
-            identityField,
-            identity,
-            protectIdentities,
-            queryAPI
-          );
+        const result = await validateNextAuth(identityField, identity, protectIdentities, queryAPI);
 
-          if (!result.success) {
-            return;
-          }
-
+        if (!result.success) {
+          token.itemId = null;
+        } else {
+          token.itemId = result.item.id;
           const data = await query[listKey].findOne({
-            where: { id: result.item.id },
+            where: { id: token.itemId },
             query: sessionData || 'id',
           });
-          const returnToken = {
-            ...token,
-            data,
-            subject: token.sub,
-            listKey,
-            itemId: result.item.id.toString(),
-          };
-
-          return returnToken;
+          token.data = data;
         }
-        return token;
+        const returnToken = {
+          ...token,
+          subject: token.sub,
+          listKey,
+        };
+
+        return returnToken;
       },
     },
   });
 }
 
-export const getNextAuthPage = (props: NextAuthPageProps) => () =>
-  NextAuthPage({ ...props });
+export const getNextAuthPage = (props: NextAuthPageProps) => () => NextAuthPage({ ...props });
