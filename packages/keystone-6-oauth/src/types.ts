476d8e01467c2062c41464a995cefb86ae389809
ScottAgirs <scott@ijs.to>
2022-06-04 10:11:13 +0300

    chore(Conform): updates types
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- packages/keystone-6-oauth/src/types.ts
+++ packages/keystone-6-oauth/src/types.ts
@@ -1,47 +1,67 @@
-import { BaseListTypeInfo, KeystoneConfig } from '@keystone-6/core/types';
+import type { ServerResponse, IncomingMessage } from 'http';
+import type { NextRequest } from 'next/server';
 import { Provider } from 'next-auth/providers';
+import { CookiesOptions, PagesOptions } from 'next-auth';
+import { BaseListTypeInfo, KeystoneConfig, CreateContext } from '@keystone-6/core/types';
 
-export type AuthGqlNames = {
-  CreateInitialInput: string;
-  createInitialItem: string;
-  authenticateItemWithPassword: string;
-  ItemAuthenticationWithPasswordResult: string;
-  ItemAuthenticationWithPasswordSuccess: string;
-  ItemAuthenticationWithPasswordFailure: string;
+type NextAuthResponse = IncomingMessage & NextRequest;
+
+export declare type AuthSessionStrategy<StoredSessionData> = {
+  start: (args: {
+    res: ServerResponse;
+    data: any;
+    createContext: CreateContext;
+  }) => Promise<string>;
+  end: (args: {
+    req: IncomingMessage;
+    res: ServerResponse;
+    createContext: CreateContext;
+  }) => Promise<void>;
+  get: (args: {
+    req: NextAuthResponse;
+    createContext: CreateContext;
+  }) => Promise<StoredSessionData | undefined>;
 };
-
-export type NextAuthSession = { listKey: string; itemId: string; data: any };
 
 export type NextAuthProviders = Provider[];
 
-type KeytoneAuthProviders = {
+type KeytoneOAuthOptions = {
   providers: NextAuthProviders;
+  pages?: Partial<PagesOptions>;
+};
+type NextAuthOptions = {
+  cookies?: Partial<CookiesOptions>;
+  resolver: any;
 };
 
-export type KeystoneAuthConfig = KeystoneConfig & KeytoneAuthProviders;
+export type KeystoneOAuthConfig = KeystoneConfig & KeytoneOAuthOptions & NextAuthOptions;
 
 export type AuthConfig<GeneratedListTypes extends BaseListTypeInfo> = {
+  /** Auth Create users in Keystone DB from Auth Provider */
+  autoCreate: boolean;
+  /** Adds ability to customize cookie options, for example, to facilitate cross-subdomain functionality */
+  cookies?: Partial<CookiesOptions>;
   /** The key of the list to authenticate users with */
   listKey: GeneratedListTypes['key'];
   /** The path of the field the identity is stored in; must be text-ish */
   identityField: GeneratedListTypes['fields'];
-  /** Session data population */
-  sessionData?: string;
-  /** Auth Create users in Keystone DB from Auth Provider */
-  autoCreate: boolean;
   /** Path for Keystone interface */
   keystonePath?: string;
-  /** Resolver for user to define their profile */
-  resolver?: Function;
+  // Custom pages for different NextAuth events
+  pages?: Partial<PagesOptions>;
   /** Providers for Next Auth */
   providers: NextAuthProviders;
+  /** Resolver for user to define their profile */
+  resolver?: (args: { user: any; profile: any; account: any }) => Promise<{
+    [key: string]: boolean | string | number;
+  }>;
+  /** Session data population */
+  sessionData?: string | undefined;
   /** Next-Auth Session Secret */
   sessionSecret: string;
 };
 
-export type AuthTokenRequestErrorCode =
-  | 'IDENTITY_NOT_FOUND'
-  | 'MULTIPLE_IDENTITY_MATCHES';
+export type AuthTokenRequestErrorCode = 'IDENTITY_NOT_FOUND' | 'MULTIPLE_IDENTITY_MATCHES';
 
 export type PasswordAuthErrorCode =
   | AuthTokenRequestErrorCode
